50ba08b2bc32e9aee5b7e3133a0dbb8b7c9c3d52
edgardjustino <eddiejustino@gmail.com>
2024-01-30 23:57:16 -0300

    adding function to JS

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const searchInput = document.getElementById('search-input');
 const resultArtists = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
+function requestApi(searchTerm){
+  fetch
+}
+
 document.addEventListener('input', function(){
   const searchTerm = searchInput.ariaValueMax.toLocaleLowerCase();
   if (searchTerm === ''){
     resultPlaylist.classList.add('hidden');
     resultArtists.classList.remove('hidden');
+    return;//q não tem nada mais pra acontecer
   }
 });
